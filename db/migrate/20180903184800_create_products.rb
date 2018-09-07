b93505d7ad0c0f953628854066dcf88386deb870
kalitkak <kalitka.kse@gmail.com>
2018-09-07 14:06:11 +0200

    migration file

--- db/migrate/20180903184800_create_products.rb
+++ db/migrate/20180903184800_create_products.rb
@@ -4,6 +4,8 @@ class CreateProducts < ActiveRecord::Migration[5.2]
       t.string :title
       t.text :description
       t.string :image_url
+      t.text :price
+      t.string :color
 
       t.timestamps
     end
---
+++ db/migrate/20180907120426_price_column.rb
@@ -0,0 +1,4 @@
+class PriceColumn < ActiveRecord::Migration[5.2]
+  def change
+  end
+end
