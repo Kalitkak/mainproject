01fa7183af88f7fdee8a6996040262fa0c6eed6c
kalitkak <kalitka.kse@gmail.com>
2018-09-15 08:50:13 +0200

    migration files

--- db/migrate/20180904175034_add_price_to_products.rb
+++ db/migrate/20180904175034_add_price_to_products.rb
@@ -1,6 +1,5 @@
 class AddPriceToProducts < ActiveRecord::Migration[5.2]
   def change
     add_column :products, :price, :decimal
-    change_column :products, :price, :decimal
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_07_122029) do
+ActiveRecord::Schema.define(version: 2018_09_15_063655) do
 
   create_table "products", force: :cascade do |t|
     t.string "title"
@@ -19,7 +19,7 @@ ActiveRecord::Schema.define(version: 2018_09_07_122029) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "colour"
-    t.string "price"
+    t.decimal "price"
   end
 
 end
