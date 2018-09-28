409454c88e6830afcb90f45ca29737b2e42cfe4f
kalitkak <kalitka.kse@gmail.com>
2018-09-28 11:09:47 +0200

    deleted USING price::numeric

--- db/migrate/20180915063655_change_price_to_decimal.rb
+++ db/migrate/20180915063655_change_price_to_decimal.rb
@@ -1,5 +1,5 @@
 class ChangePriceToDecimal < ActiveRecord::Migration[5.2]
   def change
-    change_column :products, :price, :decimal, using: 'price::numeric'
+    change_column :products, :price, :decimal
   end
 end
