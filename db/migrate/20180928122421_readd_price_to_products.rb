c188b31b2663ae1dd4ca758d5737a63e26ff3434
Ben Bagley <ben@benbagley.co.uk>
2018-10-04 17:51:50 +0100

    database and schema fix

--- db/migrate/20180928122421_readd_price_to_products.rb
+++
@@ -1,5 +0,0 @@
-class ReaddPriceToProducts < ActiveRecord::Migration[5.2]
-  def change
-    change_column :products, :price, 'decimal USING CAST (price AS decimal)'
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2018_09_15_193323) do
+
+  create_table "orders", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "product_id"
+    t.float "total"
+    t.index ["product_id"], name: "index_orders_on_product_id"
+    t.index ["user_id"], name: "index_orders_on_user_id"
+  end
+
+  create_table "products", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.string "image_url"
+    t.integer "price"
+    t.string "colour"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
