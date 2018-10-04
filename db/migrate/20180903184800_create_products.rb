ed56bd5123c695d17b9dca324ac6c45537986bbf
kalitkak <kalitka.kse@gmail.com>
2018-10-04 17:24:21 +0200

    price back

--- db/migrate/20180903184800_create_products.rb
+++ db/migrate/20180903184800_create_products.rb
@@ -4,6 +4,7 @@ class CreateProducts < ActiveRecord::Migration[5.2]
       t.string :title
       t.text :description
       t.string :image_url
+      t.integer :price
       t.string :color
       t.timestamps
     end
