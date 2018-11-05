a9df8f7c1b4f09020dc26a52dc68bb6294c414f2
kalitkak <kalitka.kse@gmail.com>
2018-11-05 20:46:37 +0100

    changes in routes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_26_143844) do
+ActiveRecord::Schema.define(version: 2018_11_05_194044) do
 
   create_table "orders", force: :cascade do |t|
     t.integer "user_id"
