1651ada865fb7adc677ab3f83274abff408b53ad
kalitkak <kalitka.kse@gmail.com>
2018-11-12 15:43:22 +0100

    form delete unnecessary

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,6 @@ Rails.application.routes.draw do
   get 'simple_pages/about'
   root 'products#index'
   get 'simple_pages/landing_page'
-
   resources :orders, only: [:index, :show, :create, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
