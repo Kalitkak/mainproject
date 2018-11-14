0ef3a1ea1d945e0685ccb3679f76a37b77ac228f
kalitkak <kalitka.kse@gmail.com>
2018-11-14 09:53:37 +0100

    sendgrid changes

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,13 @@
-class ApplicationMailer < ActionMailer::Base
-  default from: 'from@example.com'
+class ApplicationMailer < ActionMailer::Base.mail
+  default from: 'from@example.com' , to: 'to@domain.com', subject: "Welcome to My Awesome Site", body: 'I am the email body.').deliver_now
   layout 'mailer'
+  def thank_you
+  @name = params[:name]
+  @email = params[:email]
+  @message = params[:message]
+  ActionMailer::Base.mail(from: @email,
+      to: 'your-email@example.com',
+      subject: "A new contact form message from #{@name}",
+      body: @message).deliver_now
+end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,10 @@
 class Product < ApplicationRecord
   has_many :orders
   def self.search(search_term)
+  if Rails.env.production?
+    Product.where("title ilike ?", "%#{search_term}%")
+  else
     Product.where("title LIKE ?", "%#{search_term}%")
   end
+end
 end
