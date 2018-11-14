dbf333841d0a1286f2b579c8e7a65ea0d4a9dd76
kalitkak <kalitka.kse@gmail.com>
2018-11-14 10:06:15 +0100

    sendgrid changes

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,7 +1,5 @@
 class ApplicationMailer < ActionMailer::Base.mail
-  default from: 'from@example.com' , to: 'to@domain.com', subject: "Welcome to My Awesome Site", body: 'I am the email body.').deliver_now
-  layout 'mailer'
-  end
+  default from: 'from@example.com' 
   def thank_you
   @name = params[:name]
   @email = params[:email]
